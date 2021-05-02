1a9273d78877308fb414496ec1801ab6fa0b8c63
Lenovo <Lenovo@Lenovo-PC>
2021-05-02 18:13:34 +0530

    Added additional scenrio checks to the code snippy

--- CodingAssignments/src/com/vamsi/tricky/TrickyMapDemo.java
+++ CodingAssignments/src/com/vamsi/tricky/TrickyMapDemo.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class TrickyMapDemo {
 
     public static void main(String[] args) {
-	class Employee{
+	final class Employee{
 	    private int id;
 	    private String name;
 
@@ -36,9 +36,27 @@ public class TrickyMapDemo {
 	System.out.println(mapTest.put(e1, "vamsi"));
 	System.out.println(mapTest.put(e2, "sachin"));
 	
+	System.out.println(mapTest.put(e1, "Kohli"));
+	System.out.println(mapTest.put(e1, "Vamsi"));
+	
+	e1 = new Employee(12, "Yuvi");
+	
 	System.out.println(mapTest.size());
 	System.out.println(mapTest.get(e1));
 	System.out.println(mapTest.get(e2));
+	
+	Map<String,String> mapTest2 = new HashMap<>();
+	String s1 = "vamsi";
+	
+	mapTest2.put(s1, s1);
+	mapTest2.put("12", "12");
+	
+	System.out.println("Before Change :: "+mapTest2.get(s1));
+	
+	s1 = "vg";
+	
+	System.out.println("After Change :: "+mapTest2.get(s1));
+	
     }
 
 }
